d2eb60b155fa6be0b862aabe43f9c4cf0e2dd8c1
ClipLee <k.lipski777@gmail.com>
2023-11-21 11:28:53 +0100

    Refactor code and add model saving functionality

--- Lab07 - code_refactoring/task2.py
+++ Lab07 - code_refactoring/task2.py
@@ -1,13 +1,15 @@
-
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
 import pickle as pk
 import os
 
+# TODO stworzyć bardziej uniwersalny i przydatny model z wczytywaniem ścieżek domyślnej i użytkownika
+# TODO napisać aplikację konsolową, która wykorzystuje tę funkcję
 
-def save_data_and_train_model(x, y):  # przyjmuje dowolne wartosci x i y
 
+def save_data_and_train_model(x=[1, 2, 3], y=[4, 5, 6], model_path=None):
+    
     # zapisanie danych do pliku CSV
     data = np.column_stack((x, y))
     df = pd.DataFrame(data, columns=['x', 'y'])
@@ -19,8 +21,58 @@ def save_data_and_train_model(x, y):  # przyjmuje dowolne wartosci x i y
     x_train = df['x'].values.reshape(-1, 1)
     y_train = df['y'].values.reshape(-1, 1)
 
+    # domyślna ścieżka do modelu
+    default_model_path = 'Lab07 - code_refactoring/our_model.pkl'
+
+    # jeśli użytkownik nie podał ścieżki, użyj domyślnej
+    if model_path is None:
+        model_path = default_model_path
+
     # wczytanie poprzedniego i trenowanie modelu
-    model_path = 'Lab07 - code_refactoring/our_model.pkl'
+    if os.path.exists(model_path):
+        # wczytanie istniejącego modelu
+        print('✔... Model was found ...✔')
+        print('...Loading existing model...')
+        model = pk.load(open(model_path, 'rb'))
+    else:
+        # trenowanie nowego modelu
+        print('❌..Model not found..❌')
+        print('...Training new model...')
+        model = LinearRegression()
+        model.fit(x_train, y_train)
+
+    model.fit(x_train, y_train)
+
+    print('y = a * x + b')
+    print('a = ', model.coef_[0][0])
+    print('b = ', model.intercept_[0])
+
+    # zapisanie nowego modelu
+    print('...Saving model...')
+    pk.dump(model, open('Lab07 - code_refactoring/our_model.pkl', 'wb'))
+
+
+# wywołanie funkcji
+save_data_and_train_model()
+    # zapisanie danych do pliku CSV
+    data = np.column_stack((x, y))
+    df = pd.DataFrame(data, columns=['x', 'y'])
+    df.to_csv('10_points.csv', mode='a', index=False, header=False)
+
+    # wczytanie danych treningowych
+    dataset = pd.read_csv('10_points.csv')
+
+    x_train = df['x'].values.reshape(-1, 1)
+    y_train = df['y'].values.reshape(-1, 1)
+
+    # domyślna ścieżka do modelu
+    default_model_path = 'Lab07 - code_refactoring/our_model.pkl'
+
+    # jeśli użytkownik nie podał ścieżki, użyj domyślnej
+    if model_path is None:
+        model_path = default_model_path
+
+    # wczytanie poprzedniego i trenowanie modelu
     if os.path.exists(model_path):
         # wczytanie istniejącego modelu
         print('✔... Model was found ...✔')
