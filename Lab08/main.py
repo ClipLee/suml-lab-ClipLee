cdc3b4f70bfb7925166ac953b862610bdbf3b04e
ClipLee <k.lipski777@gmail.com>
2023-11-28 10:59:51 +0100

    fight with api

--- Lab08/main.py
+++ Lab08/main.py
@@ -4,12 +4,21 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from models.point import Point
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+MODEL_DIR = Path(BASE_DIR).joinpath("ml_models")
+DATA_DIR = Path(BASE_DIR).joinpath("data")
+
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/", tags=["into"])
+async def index():
+    return {"message": "Linear Regression ML"}
+
 
 
 @app.get("/items/{item_id}")
