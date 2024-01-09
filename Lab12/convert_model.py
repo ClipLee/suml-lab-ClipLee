ce84085ecbd7fd7de9f1316672b5ba953ae1e42c
ClipLee <k.lipski777@gmail.com>
2024-01-09 10:58:10 +0100

    Fix model conversion and add conversion command

--- Lab12/convert_model.py
+++ Lab12/convert_model.py
@@ -3,7 +3,7 @@ from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 
 inital_type = {
-    "X", FloatTensorType([None, 1])
+    ("X", FloatTensorType([None, 1]))
 }
 
 Model = pickle.load(open("Lab12/ml_models/our_model.pkl", "rb"))
@@ -11,3 +11,5 @@ converted_model = convert_sklearn(Model, initial_types=inital_type)
 
 with open("Lab12/ml_models/our_model.onnx", "wb") as f:
     f.write(converted_model.SerializeToString())
+
+    # odpalac konwersje za pomoca python -m onnxruntime.tools.convert_onnx_models_to_ort Lab12/ml_models/our_model.onnx
