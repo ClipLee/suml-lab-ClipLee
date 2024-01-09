ff2bde447b9fd1ae6edae3223096283b997fcc1b
ClipLee <k.lipski777@gmail.com>
2024-01-09 10:51:20 +0100

    begin lab12

---
+++ Lab12/convert_model.py
@@ -0,0 +1,13 @@
+import pickle
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+
+inital_type = {
+    "X", FloatTensorType([None, 1])
+}
+
+Model = pickle.load(open("Lab12/ml_models/our_model.pkl", "rb"))
+converted_model = convert_sklearn(Model, initial_types=inital_type)
+
+with open("Lab12/ml_models/our_model.onnx", "wb") as f:
+    f.write(converted_model.SerializeToString())
